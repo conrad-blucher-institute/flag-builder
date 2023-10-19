b024d160007d8162a5b8da6dc3ff4e9af572ffc9
matdenkas <64288827+matdenkas@users.noreply.github.com>
2023-10-19 12:55:34 -0500

    Moving changes from laptop

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,11 +1,37 @@
-import express from 'express';
+// -*- coding: utf-8 -*-
+//index.ts
+//----------------------------------
+// Created By: Matthew Kastl
+// Created Date: 1/17/2023
+// version 1.0
+//----------------------------------
+// """This file is an express webserver
+// that serves the public direcotry under root.
+//  """ 
+//----------------------------------
+// 
+//
+//Imports
 
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+//Load in .env file.
+dotenv.config();
+
+//Construct a new webapp, this is our server
 const app = express();
-app.get('/', (_req, res) => {
-  res.send('Hello world!');
-});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+//Read in what port to use from the env
+const port = process.env.PORT;
+
+//Declare the dist directory as our static dir, and host it. Anything that hits
+//the / endpoint will get the dist dir back
+const publicPath = path.join(__dirname, '../ClientBuild/pages');
+app.use(express.static(publicPath));
+
+//Start are server on the port we selected.
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
